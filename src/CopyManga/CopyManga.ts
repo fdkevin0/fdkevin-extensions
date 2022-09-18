83dc4bed40c6c44ae8a89c1d2d1f867491f4b3e8
FDKevin <fdkevinzero@gmail.com>
2022-09-18 21:02:39 +0800

    feat: update copymanga api domain

--- src/CopyManga/CopyManga.ts
+++ src/CopyManga/CopyManga.ts
@@ -18,8 +18,8 @@ import {
     Tag,
 } from 'paperback-extensions-common'
 
-const COPYMANGA_DOMAIN = 'https://www.copymanga.org'
-const COPYMANGA_API_DOMAIN = 'https://api.copymanga.com'
+const COPYMANGA_DOMAIN = 'https://copymanga.site/'
+const COPYMANGA_API_DOMAIN = 'https://api.copymanga.site'
 // Number of items requested for paged requests
 const PAGE_SIZE = 30;
 
